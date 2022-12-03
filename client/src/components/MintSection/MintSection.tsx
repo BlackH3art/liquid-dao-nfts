d1f9c86da88a357000d174245c65fdb3e04303c7
BlackH3art <jaroslaw.musielak@outlook.com>
2022-12-03 17:48:51 +0100

    add: MintSection content

--- client/src/components/MintSection/MintSection.tsx
+++ client/src/components/MintSection/MintSection.tsx
@@ -1,12 +1,48 @@
 import { FC } from "react";
+import logoNFT from '../../images/logo.jpg';
 
 export const MintSection: FC = () => {
 
   return(
-    <div className="w-full h-[100vh]">
-      <h1 className="text-4xl">
-        Mint
-      </h1>
+    <div className="w-full h-[80vh] flex items-center justify-center">
+      <div className="w-full lg:w-3/5 flex">
+
+        <div className="w-1/2 flex flex-col text-white">
+
+          <h1 className="text-8xl ">WELCOME TO</h1>
+          <h1 className="text-8xl">LIQUID+ NFTs</h1>
+
+          <p className="text-lg pt-10">Hey Team Liquid fans! We built Liquid+ NFTs just for you.</p>
+
+          <p className="text-lg pt-5">With our NFTs you can make your voice count, express your opinions and help us decide on crucial things related to our Team Liquid! Also holding our NFTs will give you a chance of winning unique merchants and participate in special Team Liquid events!</p>
+          <p className="text-lg pt-5">Join our community!</p>
+        </div>
+
+        <div className="w-1/2 flex flex-col items-center">
+          
+          <div className="flex w-full">
+            <div className="border-[1px] w-10 h-96 border-r-0 border-gray-400 ml-20"></div>
+
+            <div className="w-full flex items-center justify-center">
+              <img className="w-80 h-80 rounded-lg" src={logoNFT} alt="Logo NFT" />
+            </div>
+
+            <div className="border-[1px] w-10 h-96 border-l-0 border-gray-400 mr=20"></div>
+          </div>
+
+          <div className="flex items-center pt-10 text-white">
+            <div className="border-[1px] w-5 h-20 border-r-0 border-gray-400 ml-20"></div>
+
+            <button className="bg-gradient-to-r from-[#009dfe] h-12 w-48 to-[#007bc7] px-7 font-semibold">
+              Connect Wallet
+            </button>
+
+            <div className="border-[1px] w-5 h-20 border-l-0 border-gray-400 mr=20"></div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
