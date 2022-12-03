f5f9b2d20801892546d0bbf283c196d902eeb03c
BlackH3art <jaroslaw.musielak@outlook.com>
2022-12-03 19:04:04 +0100

    add: Navigation content

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,12 +1,22 @@
 import { FC } from "react";
+import logoSVG from '../../images/logo1.svg';
 
 export const Navigation: FC = () => {
 
   return(
-    <nav className="w-full h-24 b-2">
-      <h1 className="text-4xl">
-        navigation
-      </h1>
+    <nav className="w-full h-24 flex justify-center">
+      <div className="w-3/5 flex items-center justify-between">
+
+        <div className="w-28">
+          <img src={logoSVG} alt="logo" />
+        </div>
+
+        <div className="text-white">
+          <p>
+            0x..ff23f
+          </p>
+        </div>
+      </div>
     </nav>
   )
 }
